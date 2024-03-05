0f335583cc232572dcecffd9f9d4951f43879552
FranceCawich <L17390321@chetumal.tecnm.mx>
2024-03-04 23:56:46 -0600

    making dark light theme dynamic

--- components/DarkLight.js
+++ components/DarkLight.js
@@ -1,19 +1,32 @@
-'use client'
+"use client";
 
 import { FaCloudMoon } from "react-icons/fa";
-const DarkLight =() => {
-    return (
-        <div className="fixed top-4 right-4 w-12 h-[18px] rounded-full bg-zinc-700 flex items-center">
+import { LuSunMoon } from "react-icons/lu";
+import { useTheme } from "next-themes";
+import classNames from "classnames";
+const DarkLight = () => {
+  const { theme, setTheme } = useTheme();
 
-            <span className="w-7 h-7 flex items-center justify-center bg-black rounded-full">
-                
-                <FaCloudMoon className="text-white" size={14}/>
-                
-                </span>
-     
-        </div>
-    )
-    }
-    export default DarkLight
-
-
+  return (
+    <div
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      className="fixed top-4 right-4 w-12 h-[18px] rounded-full bg-violet-300 flex items-center"
+    >
+      <span
+        className={classNames(
+          "w-7 h-7 flex items-center justify-center bg-violet-700 rounded-full transition-all duration-500",
+          {
+            "ml-5": theme === "dark",
+          }
+        )}
+      >
+        {theme === "light" ? (
+          <FaCloudMoon className="text-white" size={14} />
+        ) : (
+          <LuSunMoon className="text-white" size={14} />
+        )}
+      </span>
+    </div>
+  );
+};
+export default DarkLight;
