e89b086f6803ad04f8d2382fd69f745fe0fe4f78
FranceCawich <L17390321@chetumal.tecnm.mx>
2024-03-14 10:11:55 -0600

    adding contact icons react icons packages

--- components/Contacts.js
+++ components/Contacts.js
@@ -1,9 +1,70 @@
 import Heading from "./Heading";
-
+import Link from "next/link";
+import data from "@/data/contacts";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+  FaDownload,
+  FaMedium,
+  FaYoutube,
+  FaFacebook,
+} from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { MdEmail } from "react-icons/md";
 const Contacts = () => {
   return (
-    <section>
+    <section className="border-t border-zinc-100 dark:border-zinc-800">
       <Heading>Contacts</Heading>
+      <div className="flex flex-wrap items-center gap-5">
+        {data.map((contact) => (
+          <Link
+            href={contact.link}
+            key={contact.id}
+            target="_blank"
+            className="flex items-center space-x-2 bg-zinc-100 dark:bg-zinc-800 px-3 py-2"
+          >
+            {contact.name === "github" && (
+              <FaGithub size={20} className="text-black dark:text-white" />
+            )}
+
+{contact.name === "twitter" && (
+              <FaTwitter size={20} className="text-black dark:text-white" />
+            )}
+
+            
+{contact.name === "linkedin" && (
+              <FaLinkedin size={20} className="text-[#0a66c2] dark:text-white" />
+            )}
+
+{contact.name === "youtube" && (
+              <FaYoutube size={20} className="text-[#ff0000] dark:text-white" />
+            )}
+
+{contact.name === "facebook" && (
+              <FaFacebook size={20} className="text-[#1877f2] dark:text-white" />
+            )}
+
+{contact.name === "medium" && (
+              <FaMedium size={20} className="text-black dark:text-white" />
+            )}
+
+{contact.name === "email" && (
+              <SiGmail size={20} className="text-red-500 dark:text-white" />
+            )}  
+
+{contact.name === "cv" && (
+              <FaDownload size={20} className="text-black dark:text-white" />
+            )}
+
+
+
+            <span className="text-base capitalize text-zinc-700 dark:text-white font-bold">
+              {contact.label}
+            </span>
+          </Link>
+        ))}
+      </div>
     </section>
   );
 };
--- components/Heading.js
+++ components/Heading.js
@@ -1,5 +1,5 @@
-const Heading = ({children }) => {
-  return <h3>{children}</h3>
+const Heading = ({children}) => {
+  return <h3 className="capitalize text-base md:text-lg text-violet-700 dark:text-zinc-400 font-bold mb-3">{children}</h3>
 }
 
 export default Heading;
