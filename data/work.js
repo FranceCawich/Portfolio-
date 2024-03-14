079d612d139f44834d00647466a87a8048cc0915
FranceCawich <L17390321@chetumal.tecnm.mx>
2024-03-14 14:15:47 -0600

    adding some static data from my work history

---
+++ data/work.js
@@ -0,0 +1,72 @@
+const data = [
+ 
+ 
+  {
+    id: 1,
+    image: "/work/estm-logo.png",
+    role: "IT Instructor ",
+    company: "Escuela Secundaria Tecna Mexico ACE Division",
+    type: "Onsite",
+    date: "06/2023 - Present",
+    skills: [
+      { id: 1, name: "Python", image: "/skills/python.png" },
+      { id: 2, name: "Html", image: "/skills/html-5.png" },
+      { id: 3, name: "mariaDb", image: "/skills/mariadb.png" },
+
+    ],
+  }, {
+    id: 2,
+    image: "/work/QuickStop.png",
+    company: "Quickstop Finance Center",
+    role: "full Stack Developer",
+    type: "Onsite",
+    date: "20/02/2023 - 20/11/2023",
+    skills: [
+      { id: 1, name: "node JS", image: "/skills/node-js.png" },
+      { id: 2, name: "express JS", image: "/skills/express.png" },
+      { id: 3, name: "mongoDB", image: "/skills/mongodb.png" },
+      { id: 4, name: "react JS", image: "/skills/react.png" },
+      { id: 5, name: "Next JS", image: "/skills/next-js.png" },
+      //{ id: 6, name: "redux", image: "/skills/redux.png" },
+      { id: 7, name: "tailwind CSS", image: "/skills/tailwind.png" },
+      { id: 8, name: "material UI", image: "/skills/mui.png" },
+      { id: 9, name: "github", image: "/skills/git.png" },
+    ],
+  },
+
+  {
+    id: 3,
+    image: "/work/Itche.png",
+    role: "Reasearch and Development Intern",
+    company: "Unversity Institute of technology of Chetumal",
+    type: "onsite",
+    date: "08/2019 - 11/2022",
+    skills: [
+      { id: 1, name: "Python", image: "/skills/python.png" },
+      { id: 2, name: "Pytorch", image: "/skills/pythorch.png" },
+      { id: 3, name: "tensorflow", image: "/skills/Tensorflow.png" },
+      { id: 4, name: "DeepLearing", image: "/skills/dp.jpg" },
+      { id: 5, name: "mongoDB", image: "/skills/mongodb.png" },
+      { id: 6, name: "github", image: "/skills/git.png" },
+    ],
+  },
+
+  {
+    id: 4,
+    image: "/work/freelancer.png",
+    role: "DataAnalyst",
+    type: "online",
+    date: "5/2021 - 2022",
+    skills: [
+      { id: 1, name: "Python", image: "/skills/python.png" },
+      { id: 2, name: "Pytorch", image: "/skills/pythorch.png" },
+      { id: 3, name: "tensorflow", image: "/skills/Tensorflow.png" },
+      { id: 4, name: "DeepLearing", image: "/skills/dp.jpg" },
+      { id: 5, name: "mongoDB", image: "/skills/mongodb.png" },
+      { id: 6, name: "github", image: "/skills/git.png" },
+    ],
+
+  },
+
+];
+export default data;
