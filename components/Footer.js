c73751dd903bcb6cc38e4ec6c91e749c78046a8f
FranceCawich <L17390321@chetumal.tecnm.mx>
2024-03-28 22:06:27 -0600

    updating resume file

--- components/Footer.js
+++ components/Footer.js
@@ -6,8 +6,7 @@ const Footer = () => {
     <footer className="border-t border-t-zinc-100 dark:border-t-zinc-800 mt-10">
       <div className="max-w-screen-xl mx-auto px-4 py-5">
         <p className="text-sm font-semibold text-center text-zinc-800 dark:text-zinc-400">
-          Developed by <strong className="font-black">Francis Cawich</strong> powered by 
-          <span className="flex items-center justify-center">
+          Developed by <strong className="font-black">Francis Cawich</strong> powered by<span className="flex items-center justify-center">
             <SiVercel className="inline-block mr-1" /> Vercel
             <TbBrandNextjs className="inline-block mr-1" /> Next.js
           </span> 
